58cf34c87686b88e6d58f503a66ae5258e5fc210
MariaIP28 <mariapashkulova28@gmail.com>
2024-04-30 17:14:04 +0300

    Add react icons and change Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,10 @@ import { Flex, Box, Heading, Text, Button, Spacer } from "@chakra-ui/react";
 
 export default function Navbar() {
     return (
-        <Flex as='nav' mb='4' py='15px' px='30px' alignItems="center" gap='20px'>
+
+        <Flex justifyContent='flex-end' as='nav' mb='4' py='4' px='8' alignItems="center" gap='5'>
             {/* <Heading>Хоби Кошер</Heading> */}
-            <Spacer />
+            {/* <Spacer /> */}
 
             <Box bg='gray.200' p='10px' >MП</Box>
             <Text>mpashkulova@gmail.com</Text>
