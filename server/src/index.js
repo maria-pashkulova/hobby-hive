28a5ebd445c354f5e48a91f91708220b2e6d5860
MariaIP28 <mariapashkulova28@gmail.com>
2024-05-13 15:33:20 +0300

    add comment

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config(); //прави process.env обекта глобално достъпен
 
 
 const expressConfigurator = require('./config/expressConfigurator');
