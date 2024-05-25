f7acb5a30f4006631c80f252e84fd2837d459ac1
MariaIP28 <mariapashkulova28@gmail.com>
2024-05-26 00:17:09 +0300

    Use abstract request in groupService

--- client/src/services/groupService.js
+++ client/src/services/groupService.js
@@ -1,31 +1,8 @@
+import * as request from '../lib/request';
+
 const baseUrl = 'http://localhost:5000';
 
 
-export const getAll = async () => {
+export const getAll = async () => request.get(baseUrl);
 
-    const response = await fetch(baseUrl, { credentials: 'include' });
-
-    //Опит за заявка ако не сме се логнали
-    //type:cors?? -> получавам грешки в конзолата??
-    //console.log(response);
-    if (response.status == 401) {
-        throw new Error('Unathenticated');
-    }
-    const result = await response.json();
-
-    return result;
-}
-
-export const getById = async (groupId) => {
-    const response = await fetch(`${baseUrl}/groups/${groupId}`, { credentials: 'include' });
-
-    //Опит за заявка ако не сме се логнали
-    //type:cors?? -> получавам грешки в конзолата??
-    //console.log(response);
-    if (response.status == 401) {
-        throw new Error('Unathenticated');
-    }
-    const result = await response.json();
-
-    return result;
-}
+export const getById = async (groupId) => request.get(`${baseUrl}/groups/${groupId}`);
