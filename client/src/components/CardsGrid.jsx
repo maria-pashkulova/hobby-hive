4b4f9d2c649c3426c6ed3ebe326ea29737e2b5e7
MariaIP28 <mariapashkulova28@gmail.com>
2024-06-05 11:36:34 +0300

    Separate card grid as a component in order to be reused

---
+++ client/src/components/CardsGrid.jsx
@@ -0,0 +1,51 @@
+import { Card, CardBody, CardFooter, Heading, SimpleGrid, Button, Text, Flex, Image, Spacer } from '@chakra-ui/react'
+import { Link } from "react-router-dom";
+
+
+const CardsGrid = ({ groups, partialLinkToGroup }) => {
+    return (
+        <SimpleGrid spacing={4} templateColumns={{ base: '1fr', xl: 'repeat(2,1fr)' }}>
+
+            {groups.map((group, i) => (
+                <Card
+                    key={i}
+                    direction={{ base: 'column', md: 'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                >
+                    <Image
+                        objectFit='cover'
+                        maxW={{ base: '100%', md: '200px' }}
+                        src={group.imageUrl}
+                        alt={group.name}
+                    />
+
+                    <Flex direction='column' width='100%' >
+                        <CardBody>
+                            <Heading size='md'>{group.name}</Heading>
+
+                            <Text py='2'>
+                                {group.description}
+                            </Text>
+                        </CardBody>
+
+                        <CardFooter alignItems='center' gap='4' >
+                            <Button as={Link} to={`${partialLinkToGroup}/${group._id}`} variant='solid' colorScheme='blue'>
+                                Към групата
+                            </Button>
+                            <Spacer />
+                            <Text as='span'>
+                                Членове: {group.membersCount}
+                            </Text>
+                        </CardFooter>
+                    </Flex>
+                </Card>
+
+            ))}
+
+        </SimpleGrid>
+
+    )
+}
+
+export default CardsGrid;
--- client/src/pages/MyGroupsPage.jsx
+++ client/src/pages/MyGroupsPage.jsx
@@ -7,6 +7,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import * as authService from '../services/authService';
 import AuthContext from "../contexts/authContext";
+import CardsGrid from "../components/CardsGrid";
 
 const MyGroupsPage = () => {
 
@@ -56,50 +57,7 @@ const MyGroupsPage = () => {
 
             </Flex>
 
-
-            {/* My Groups + Create Group Button */}
-            <SimpleGrid spacing={4} templateColumns={{ base: '1fr', xl: 'repeat(2,1fr)' }}>
-
-                {groups.map((group, i) => (
-                    <Card
-                        key={i}
-                        direction={{ base: 'column', md: 'row' }}
-                        overflow='hidden'
-                        variant='outline'
-                    >
-                        <Image
-                            objectFit='cover'
-                            maxW={{ base: '100%', md: '200px' }}
-                            src={group.imageUrl}
-                            alt={group.name}
-                        />
-
-                        <Flex direction='column' width='100%' >
-                            <CardBody>
-                                <Heading size='md'>{group.name}</Heading>
-
-                                <Text py='2'>
-                                    {group.description}
-                                </Text>
-                            </CardBody>
-
-                            <CardFooter alignItems='center' gap='4' >
-                                {/* use absolute path instead of relative one here */}
-                                <Button as={Link} to={`/groups/${group._id}`} variant='solid' colorScheme='blue'>
-                                    Към групата
-                                </Button>
-                                <Spacer />
-                                <Text as='span'>
-                                    Членове: {group.membersCount}
-                                </Text>
-                            </CardFooter>
-                        </Flex>
-                    </Card>
-
-                ))}
-
-            </SimpleGrid>
-
+            <CardsGrid groups={groups} partialLinkToGroup='/groups' />
 
         </>
     )
