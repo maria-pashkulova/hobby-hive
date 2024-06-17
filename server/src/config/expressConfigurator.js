4af7ecf51c950f107c6c92361314135bd4cab305
MariaIP28 <mariapashkulova28@gmail.com>
2024-06-17 20:35:56 +0300

    handle payload too large error if bigger image is uploaded

--- server/src/config/expressConfigurator.js
+++ server/src/config/expressConfigurator.js
@@ -5,15 +5,6 @@ const cookieParser = require('cookie-parser');
 
 
 function expressConfig(app) {
-    //обработва данни от форми с метод POST и querystrings също
-    //в случай на SPA не мисля че се ползва - всъщност се ползва ако искаме
-    //да имаме querystrings в req.query
-    //в случай на SPA НЕ служи за да парсва form-urlencoded данни идващи от форма с action - 
-    //endpoint на сървъра и метод POST, а само за парсване на querystrings -> req.query
-    app.use(express.urlencoded({ extended: false }));
-    //Обработва данни от форма идващи в json формат - клиентът
-    //трябва да ги оформи под формата на json (Реакт ще се грижи за това)
-    app.use(express.json()); // To parse JSON data in the req.body
 
     //CORS related
     app.use((req, res, next) => {
@@ -33,6 +24,16 @@ function expressConfig(app) {
         }
     })
 
+    //обработва данни от форми с метод POST и querystrings също
+    //в случай на SPA не мисля че се ползва - всъщност се ползва ако искаме
+    //да имаме querystrings в req.query
+    //в случай на SPA НЕ служи за да парсва form-urlencoded данни идващи от форма с action - 
+    //endpoint на сървъра и метод POST, а само за парсване на querystrings -> req.query
+    app.use(express.urlencoded({ extended: false }));
+    //Обработва данни от форма идващи в json формат - клиентът
+    //трябва да ги оформи под формата на json (Реакт ще се грижи за това)
+    app.use(express.json({ limit: '10mb' })); // To parse JSON data in the req.body
+
     //Working with cookies
     app.use(cookieParser());
 
@@ -40,6 +41,17 @@ function expressConfig(app) {
     app.use((req, res, next) => {
         console.log(req.method, req.path);
         next();
+    });
+
+    //Error handling middleware for PayloadTooLargeError that can be thrown by 
+    //express.urlEncoded() and express.json() middlewares
+    app.use((err, req, res, next) => {
+        if (err.status === 413) {
+            //явно req.json() и req.urlEncoded() си закачат именно статус 413 като хвърлят грешка
+            res.status(413).json({ message: 'Payload too large' })
+        } else {
+            next(err)
+        }
     })
 }
 
