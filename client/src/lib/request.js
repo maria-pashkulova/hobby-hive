497c5f327276b0b9692744ebd75ff53deb8ca2a1
MariaIP28 <mariapashkulova28@gmail.com>
2025-04-28 16:43:57 +0300

    Remove redundant try-catch block

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,75 +1,62 @@
 const buildOptions = (data) => {
-    const options = {};
+  const options = {};
 
-    if (data) {
-        options.body = JSON.stringify(data);
-        options.headers = {
-            'Content-Type': 'application/json'
-        }
-    }
+  if (data) {
+    options.body = JSON.stringify(data);
+    options.headers = {
+      "Content-Type": "application/json",
+    };
+  }
 
-    return options;
-}
+  return options;
+};
 
 const request = async (method, url, data) => {
+  // Perform the fetch operation
+  const response = await fetch(url, {
+    ...buildOptions(data),
+    method,
+    credentials: "include", //this ensures cookies are sent and received
+  });
 
-    try {
-        // Perform the fetch operation
-        const response = await fetch(url, {
-            ...buildOptions(data),
-            method,
-            credentials: 'include' //this ensures cookies are sent and received
-        });
+  // Check if the response status is not ok (status code not in the range 200-299)
+  if (response.ok == false) {
+    // Parse the response body as JSON to get the error message
+    const error = await response.json();
 
-        // Check if the response status is not ok (status code not in the range 200-299)
-        if (response.ok == false) {
-            // Parse the response body as JSON to get the error message
-            const error = await response.json();
-
-            // Throw an error with the message from the server and status - custom errors logic
-            //Check is needed because of Google API errors 
-            if (error.message) {
-                const errorWithStatus = new Error(error.message)
-                errorWithStatus.status = response.status;
-                throw errorWithStatus;
-            }
-
-            //Google API errors thrown by googleapis library
-            throw error;
-
-        }
-
-        //204 - no content -> logout server response and successful delete responses
-        if (response.status === 204) {
-            return {};
-        }
-
-        //TODO: check other satus codes?
-
-        try {
-            // Try to parse the response body as JSON
-            const result = await response.json();
-            // Return the parsed JSON data
-            return result;
-
-        } catch (error) {
-            // If parsing as JSON fails, return the raw response
-            return response;
-        }
-
-    } catch (error) {
-        // If any error occurs during fetch or JSON parsing
-        // re-throw the error so it can be handled by the caller of this function
-        throw error;
+    // Throw an error with the message from the server and status - custom errors logic
+    //Check is needed because of Google API errors
+    if (error.message) {
+      const errorWithStatus = new Error(error.message);
+      errorWithStatus.status = response.status;
+      throw errorWithStatus;
     }
 
+    //Google API errors thrown by googleapis library
+    throw error;
+  }
 
-}
+  //204 - no content -> logout server response and successful delete responses
+  if (response.status === 204) {
+    return {};
+  }
 
+  //TODO: check other satus codes?
+
+  try {
+    // Try to parse the response body as JSON
+    const result = await response.json();
+    // Return the parsed JSON data
+    return result;
+  } catch (error) {
+    // If parsing as JSON fails, return the raw response
+    return response;
+  }
+};
 
 //partial application; създаваме си интерфейс подобен на този на axios
 //с тази разлика че сами си пишем фетч заявките и знаем какво точно се случва
-export const get = request.bind(null, 'GET');
-export const post = request.bind(null, 'POST');
-export const put = request.bind(null, 'PUT');
-export const remove = request.bind(null, 'DELETE');
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const remove = request.bind(null, "DELETE");
